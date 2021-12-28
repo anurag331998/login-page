9023cf735c681c48ee1b528857835735f6e3012b
anurag3398 <96459791+anurag3398@users.noreply.github.com>
2021-12-28 16:32:04 +0530

    Login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Button from './Components/Button';
+import Login from './Components/login';
+import { header, buttons, words } from './Components/Header.css';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='header'>POSTEN WEBPAGE</div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <Login></Login>
     </div>
+
   );
 }
-
 export default App;
